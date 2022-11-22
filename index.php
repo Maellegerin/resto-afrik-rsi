41a0a311c40686bf98e0a24b0dc83f53359f3092
Maellegerin <118839624+Maellegerin@users.noreply.github.com>
2022-11-22 19:17:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">   
+        <title>Accueil</title>
+    </head>
+    <body>
+        <header><?php Bienvenue au cours de PHP; ?></header>
+        
+        <section>
+        <?php
+            echo "j'espère que vous prendrez autant du plaisir à apprendre";
+            echo "que j'en ai eu à vous préparer ce cours";   
+        ?> 
+        </section>
+
+        <footer>
+            <address>bice@gmail.com</address>
+        </footer>        
+    </body>
+</html>
+
